8a8a0a3cd4eaa9308fe206ce1a9ec1144def1ea2
bigazzzz <bigazzzz@gmail.com>
2023-09-25 17:14:21 +0300

    version 0.1

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import uvicorn
 
-from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=JSONResponse)
+@app.get("/", response_class=PlainTextResponse)
 async def index(request: Request):
-    return JSONResponse(sys.argv[1:])
+    return templates.TemplateResponse("json_array.txt.j2", {"request": request, "envs": get_env_array()})
 
 
 @app.get("/all", response_class=JSONResponse)
-async def index(request: Request):
-    envs = []
-    for env in os.environ:
-        envs.append({"name": str(env), "value": str(os.environ[env])})
+async def get_all_env(request: Request):
+    return JSONResponse(get_env_array())
 
-    return JSONResponse(envs)
+@app.get("/hostname", response_class=PlainTextResponse)
+async def get_hostname(request: Request):
+    return PlainTextResponse(os.uname().nodename)
 
 
-@app.get("/env/{env}", response_class=JSONResponse)
+@app.get("/env/{env}", response_class=PlainTextResponse)
 async def get_env(request: Request, env:str):
-    envs = { str(env): str(os.environ[env])}
-    return JSONResponse(envs)
+    return PlainTextResponse(str(os.environ[env]))
+
+def get_env_array():
+    envs = dict()
+    for env in os.environ:
+        envs[str(env)] = str(os.environ[env])
+    return envs
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5000, log_level="info", reload=True)
